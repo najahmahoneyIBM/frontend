ebd0a319c81ae95d40146af634cd365720d56021
najahmahoneyIBM <najah.mahoney@ibm.com>
2022-04-11 12:58:34 -0500

    removed noise

--- src/screens/LandingPage/LandingPage.js
+++ src/screens/LandingPage/LandingPage.js
@@ -20,7 +20,7 @@ function LandingPage({ history }) {
         <Row>
           <div className="intro-text">
             <div>
-              <h1 className="title">Virtu Admin %%%% Najah Mahoney cloud premise New Page</h1>
+              <h1 className="title">Virtu Admin</h1>
               <h2 className="subtitle"></h2>
             </div>
             <div className="buttonContainer">
